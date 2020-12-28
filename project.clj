daf742055436d4b3f9f6c6038f393a90cb8fb90b
Daniel Fitzpatrick <crinklywrappr@pm.me>
2020-12-28 10:44:01 -0600

    changing the maven ns

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def jetty-version "9.4.35.v20201120")
 
-(defproject info.sunng/ring-jetty9-adapter "0.14.2"
+(defproject crinklywrappr/ring-jetty9-adapter "0.14.2"
   :description "Ring adapter for jetty9, which supports websocket and spdy. This is a fork with better ping/pong support.  Follow the upstream commit here: https://github.com/sunng87/ring-jetty9-adapter/pull/55"
   :url "http://github.com/sunng87/ring-jetty9-adapter"
   :license {:name "Eclipse Public License"
